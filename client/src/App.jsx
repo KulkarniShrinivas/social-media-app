79adac7e327d22b98714fa0b01194c3b5b7dbae6
Shrinivas Kulkarni <shrinivaskulkarni989@gmail.com>
2023-11-14 22:48:16 +0530

    updated authentication and autherization

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,13 +16,17 @@ function App() {
   const mode = useSelector((state) => state.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
 
+  //if the token exists we are autherized 
+  const isAuth = Boolean(useSelector((state) => state.token))
+
   return (
     <BrowserRouter>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <Routes>
           <Route path="/" element={<LoginPage />} />
-          <Route path="/home" element={<HomePage />} />
+          <Route path="/home"  element={isAuth ? <HomePage /> : <Navigate to="/" />} />
+          <Route path="/profile/:userId"  element={isAuth ? <ProfilePage /> : <Navigate to="/" />} />
         </Routes>
       </ThemeProvider>
     </BrowserRouter>
